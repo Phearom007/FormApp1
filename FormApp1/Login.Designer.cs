dc0d2656697c5e9ac3f5fdae7126e388ea6fb22f
Phearom007 <phearomkeo73@gmail.com>
2022-01-02 18:33:40 +0700

    Add project files.

---
+++ FormApp1/Login.Designer.cs
@@ -0,0 +1,196 @@
+﻿
+namespace FormApp1
+{
+    partial class BasicForm1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new Panel();
+            this.lblpanel = new System.Windows.Forms.Label();
+            this.btnexit = new System.Windows.Forms.Button();
+            this.pic1 = new System.Windows.Forms.PictureBox();
+            this.txtname = new System.Windows.Forms.TextBox();
+            this.lblname = new System.Windows.Forms.Label();
+            this.lblusername = new System.Windows.Forms.Label();
+            this.txtusername = new System.Windows.Forms.TextBox();
+            this.btnlogin = new System.Windows.Forms.Button();
+            this.btnregister = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pic1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.panel1.Controls.Add(this.lblpanel);
+            this.panel1.Controls.Add(this.btnexit);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1228, 200);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.Panel1_Paint);
+            // 
+            // lblpanel
+            // 
+            this.lblpanel.AutoSize = true;
+            this.lblpanel.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblpanel.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblpanel.Location = new System.Drawing.Point(294, 74);
+            this.lblpanel.Name = "lblpanel";
+            this.lblpanel.Size = new System.Drawing.Size(177, 45);
+            this.lblpanel.TabIndex = 3;
+            this.lblpanel.Text = "Panel User";
+            this.lblpanel.Click += new System.EventHandler(this.Label1_Click);
+            // 
+            // btnexit
+            // 
+            this.btnexit.BackColor = System.Drawing.Color.Red;
+            this.btnexit.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnexit.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnexit.Location = new System.Drawing.Point(1092, 29);
+            this.btnexit.Name = "btnexit";
+            this.btnexit.Size = new System.Drawing.Size(104, 56);
+            this.btnexit.TabIndex = 4;
+            this.btnexit.Text = "X";
+            this.btnexit.UseVisualStyleBackColor = false;
+            this.btnexit.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // pic1
+            // 
+            this.pic1.Image = global::FormApp1.Properties.Resources.photo_2021_12_28_21_48_31;
+            this.pic1.Location = new System.Drawing.Point(0, 0);
+            this.pic1.Name = "pic1";
+            this.pic1.Size = new System.Drawing.Size(251, 200);
+            this.pic1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pic1.TabIndex = 1;
+            this.pic1.TabStop = false;
+            // 
+            // txtname
+            // 
+            this.txtname.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtname.Location = new System.Drawing.Point(433, 281);
+            this.txtname.Name = "txtname";
+            this.txtname.Size = new System.Drawing.Size(384, 50);
+            this.txtname.TabIndex = 2;
+            this.txtname.TextChanged += new System.EventHandler(this.TextBox1_TextChanged);
+            // 
+            // lblname
+            // 
+            this.lblname.AutoSize = true;
+            this.lblname.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblname.Location = new System.Drawing.Point(135, 292);
+            this.lblname.Name = "lblname";
+            this.lblname.Size = new System.Drawing.Size(187, 45);
+            this.lblname.TabIndex = 3;
+            this.lblname.Text = "Enter Name";
+            this.lblname.Click += new System.EventHandler(this.Label1_Click);
+            // 
+            // lblusername
+            // 
+            this.lblusername.AutoSize = true;
+            this.lblusername.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblusername.Location = new System.Drawing.Point(135, 375);
+            this.lblusername.Name = "lblusername";
+            this.lblusername.Size = new System.Drawing.Size(245, 45);
+            this.lblusername.TabIndex = 3;
+            this.lblusername.Text = "Enter Username";
+            this.lblusername.Click += new System.EventHandler(this.Label1_Click);
+            // 
+            // txtusername
+            // 
+            this.txtusername.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtusername.Location = new System.Drawing.Point(433, 368);
+            this.txtusername.Name = "txtusername";
+            this.txtusername.Size = new System.Drawing.Size(384, 50);
+            this.txtusername.TabIndex = 2;
+            this.txtusername.TextChanged += new System.EventHandler(this.TextBox1_TextChanged);
+            // 
+            // btnlogin
+            // 
+            this.btnlogin.BackColor = System.Drawing.Color.Navy;
+            this.btnlogin.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnlogin.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnlogin.Location = new System.Drawing.Point(545, 458);
+            this.btnlogin.Name = "btnlogin";
+            this.btnlogin.Size = new System.Drawing.Size(150, 56);
+            this.btnlogin.TabIndex = 4;
+            this.btnlogin.Text = "Login";
+            this.btnlogin.UseVisualStyleBackColor = false;
+            // 
+            // btnregister
+            // 
+            this.btnregister.BackColor = System.Drawing.Color.Navy;
+            this.btnregister.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnregister.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnregister.Location = new System.Drawing.Point(545, 550);
+            this.btnregister.Name = "btnregister";
+            this.btnregister.Size = new System.Drawing.Size(150, 66);
+            this.btnregister.TabIndex = 4;
+            this.btnregister.Text = "Register";
+            this.btnregister.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1228, 677);
+            this.Controls.Add(this.btnregister);
+            this.Controls.Add(this.btnlogin);
+            this.Controls.Add(this.lblusername);
+            this.Controls.Add(this.lblname);
+            this.Controls.Add(this.txtusername);
+            this.Controls.Add(this.txtname);
+            this.Controls.Add(this.pic1);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "LoginForm";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pic1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private PictureBox pic1;
+        private TextBox txtname;
+        private Label lblname;
+        private Label lblusername;
+        private Label lblpanel;
+        private TextBox txtusername;
+        private Button btnlogin;
+        private Button btnregister;
+        private Button btnexit;
+    }
+}
---
+++ FormApp1/Login.cs
@@ -0,0 +1,35 @@
+namespace FormApp1
+{
+    public partial class BasicForm1 : Form
+    {
+    public BasicForm1()
+        {
+            InitializeComponent();
+        }
+
+        private void Panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
